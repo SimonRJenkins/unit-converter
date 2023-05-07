7242f560272d5eeb69e6eb772caa4477c86db096
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-05-07 11:19:22 +0100

    Added input text to the unit user is converting from
    e.g input meters = feet | input feet = meters

--- index.js
+++ index.js
@@ -4,4 +4,45 @@
 1 kilogram = 2.204 pound
 */
 
-const inputEl = document.getElementById("input-el")
+let inputEl = document.getElementById("input-el")
+let convertBtn = document.getElementById("convert-btn")
+
+let lengthResult = document.getElementById("length-result")
+let volumeResult = document.getElementById("volume-result")
+let massResult = document.getElementById("mass-result")
+
+let metersFeet = document.getElementById("meters")
+let feetMeters = document.getElementById("feet")
+
+let liters = document.getElementById("liters")
+let gallons = document.getElementById("gallons")
+
+let kilos = document.getElementById("kilos")
+let pounds = document.getElementById("pounds")
+
+function convert() {
+
+}
+
+convertBtn.addEventListener("click", function () {
+    meters.textContent += inputEl.value + " ";
+    feet.textContent += inputEl.value + " ";
+    kilos.textContent += inputEl.value + " ";
+    pounds.textContent += inputEl.value + " ";
+    liters.textContent += inputEl.value + " ";
+    gallons.textContent += inputEl.value + " ";
+
+
+})
+
+
+function meterToFeet() {
+    //metersFeet.textContent = inputEl.value * 3.281
+}
+
+
+
+
+
+
+
